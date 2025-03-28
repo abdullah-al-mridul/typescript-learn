bbc79ca58c1b6db4851793b1eb879428b8221546
Abdullah Al Mridul <abdullah.github@zohomail.com>
2025-03-28 20:47:17 +0600

    learned implicit explicit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// there are two main ways to defiene type in typescript...
+// 1. explicit
+// 2. implicit
+// explicit type
+const fullName = "John Doe";
+const age = 30;
+const isEmployed = true;
+// console.log(fullName, age, isEmployed);
+// implicit type
+const fullNameImplicit = "John Doe"; // guess string
+const ageImplicit = 30; // guess number
+const isEmployedImplicit = true; // guess boolean
+// console.log(fullNameImplicit, ageImplicit, isEmployedImplicit);
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// there are two main ways to defiene type in typescript...
+
+// 1. explicit
+// 2. implicit
+
+// explicit type
+
+const fullName: string = "John Doe";
+const age: number = 30;
+const isEmployed: boolean = true;
+
+// console.log(fullName, age, isEmployed);
+
+// implicit type
+
+const fullNameImplicit = "John Doe"; // guess string
+const ageImplicit = 30; // guess number
+const isEmployedImplicit = true; // guess boolean
+
+// console.log(fullNameImplicit, ageImplicit, isEmployedImplicit);
