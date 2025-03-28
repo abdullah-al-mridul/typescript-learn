00a1325fdf71c2c7676638d7243737cc60b23793
Abdullah Al Mridul <abdullah.github@zohomail.com>
2025-03-28 21:03:13 +0600

    arrays

--- src/index.js
+++ src/index.js
@@ -18,3 +18,17 @@ let isEmployedImplicit = true; // guess boolean
 let anyType = "John Doe";
 anyType = 30; // no error cause we have defined anyType as any
 // console.log(anyType);
+//arrays
+//this is a string array
+const names = ["John", "Doe", "Jane"];
+//this is a number array
+const ages = [30, 25, 35];
+//mixed array betwen string and number
+const mixedArray = ["John", 10];
+//this is a tuple array
+const tupleArray = ["str", 0];
+const tupleArray2 = [
+    ["str", 0, true],
+    ["str", 0, false],
+];
+// console.log(names, ages, mixedArray, tupleArray, tupleArray2);
--- src/index.ts
+++ src/index.ts
@@ -27,3 +27,20 @@ let anyType: any = "John Doe";
 anyType = 30; // no error cause we have defined anyType as any
 
 // console.log(anyType);
+
+//arrays
+
+//this is a string array
+const names: string[] = ["John", "Doe", "Jane"];
+//this is a number array
+const ages: number[] = [30, 25, 35];
+//mixed array betwen string and number
+const mixedArray: (string | number)[] = ["John", 10];
+//this is a tuple array
+const tupleArray: [string, number] = ["str", 0];
+const tupleArray2: [string, number, boolean][] = [
+  ["str", 0, true],
+  ["str", 0, false],
+];
+
+// console.log(names, ages, mixedArray, tupleArray, tupleArray2);
